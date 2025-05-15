1c9a8a7593536719454bfbce887e0041908da33c
hmt <97658991+kaungkhanthein1@users.noreply.github.com>
2025-05-15 13:47:29 +0700

    update

--- src/components/home/Carousel.jsx
+++ src/components/home/Carousel.jsx
@@ -2,52 +2,38 @@ import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import test from "./test.png";
-import { Autoplay } from "swiper/modules"; // Import Swiper's autoplay module
-
+import { Autoplay } from "swiper/modules";
 
 const Carousel = () => {
-  const [spaceBetween, setSpaceBetween] = useState(150);
   const [selectedIndex, setSelectedIndex] = useState(0);
-
 
   const handleOnChange = (swiper) => {
     setSelectedIndex(swiper.realIndex);
   };
 
-  useEffect(() => {
-    const updateSpace = () => {
-      const width = window.innerWidth;
-      if (width >= 390) {
-        setSpaceBetween(100);
-      } else {
-        setSpaceBetween(120);
-      }
-    };
-    updateSpace();
-    window.addEventListener("resize", updateSpace);
-    return () => window.removeEventListener("resize", updateSpace);
-  }, []);
   return (
-    <div>
+    <div className="relative">
       <Swiper
-        className=""
+        className="w-full"
         slidesPerView={1.2}
-        spaceBetween={15}
         centeredSlides={true}
         loop={true}
         autoplay={{ delay: 3000, disableOnInteraction: false }}
         modules={[Autoplay]}
         onSlideChange={handleOnChange}
+        spaceBetween={3}
       >
-        <SwiperSlide className=" w-full ">
-          <img src={test} alt="" />
-        </SwiperSlide>
-        <SwiperSlide className=" w-full ">
-          <img src={test} alt="" />
-        </SwiperSlide>
-        <SwiperSlide className=" w-full ">
-          <img src={test} alt="" />
-        </SwiperSlide>
+        {[0, 1, 2, 3].map((_, index) => (
+  <SwiperSlide key={index} className="w-full transition-all duration-300">
+    <img
+      src={test}
+      alt=""
+      className={`w-full h-full object-cover rounded-lg transition-transform duration-300 ${
+        selectedIndex === index ? "scale-100" : "scale-95 opacity-80"
+      }`}
+    />
+  </SwiperSlide>
+))}
       </Swiper>
     </div>
   );
