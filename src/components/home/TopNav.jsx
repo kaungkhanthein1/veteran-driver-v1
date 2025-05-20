9aba7c14ed8d135fc08d8437a782b6d219be3b5b
hmt <97658991+kaungkhanthein1@users.noreply.github.com>
2025-05-20 11:52:11 +0700

    update

--- src/components/home/TopNav.jsx
+++ src/components/home/TopNav.jsx
@@ -8,10 +8,10 @@ const TopNav = () => {
   const navigate = useNavigate()
 
   return (
-    <div className="px-[16px] py-[20px] flex justify-between w-full items-center bg-white">
+    <div className="px-[16px] py-[20px] flex justify-between w-full items-center bg-theme-secondary">
       <div className="flex justify-center items-center">
         <img src={thai} alt="" />
-        <span className="text-gray-800 text-[16px] font-[500] leading-[20px] pl-[8px] pr-[6px]">
+        <span className="text-theme-primary text-[16px] font-[500] leading-[20px] pl-[8px] pr-[6px]">
           Thailand
         </span>
         <svg
@@ -24,20 +24,20 @@ const TopNav = () => {
           <path
             d="M8.825 0.912109L5 4.72878L1.175 0.912109L0 2.08711L5 7.08711L10 2.08711L8.825 0.912109Z"
             fill="currentColor"
-            className="text-gray-800"
+            className="text-theme-primary"
           />
         </svg>
       </div>
       {/* icons */}
       <div className="flex gap-[12px]">
-        <div onClick={() => navigate("/map")} className="bg-gray-100 rounded-full p-[12px]">
+        <div onClick={() => navigate("/map")} className="bg-theme-primary rounded-full p-[12px]">
           <img 
             src={search} 
             alt="" 
             className="[filter:var(--icon-filter)]"
           />
         </div>
-        <div className="bg-gray-100 rounded-full p-[12px]">
+        <div className="bg-theme-primary rounded-full p-[12px]">
           <img 
             src={map} 
             alt="" 
