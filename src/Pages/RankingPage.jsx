269c95e03497e56a4587d4b6e0fa1d27229c01f2
hmt <97658991+kaungkhanthein1@users.noreply.github.com>
2025-06-07 17:07:24 +0700

    update

--- src/Pages/RankingPage.jsx
+++ src/Pages/RankingPage.jsx
@@ -133,136 +133,138 @@ export default function RankingPage() {
   };
 
   return (
-    <div className="min-h-screen flex flex-col bg-theme-primary">
+    <div className="dvh-fallback flex flex-col">
       {/* Header Section */}
-      <div className="h-[150px] w-full sticky top-0 z-40" style={{ backgroundColor: '#271D00CC' }}>
+      <div className="h-[150px] w-full sticky top-0 z-40" style={{ backgroundColor: '#271D0099' }}>
         <img
           src={RankingHeader}
           alt="Ranking Header Background"
           className="absolute inset-0 w-full h-full object-cover"
         />
-        <div className="absolute inset-0 w-full h-full" style={{ backgroundColor: '#271D00CC' }}></div>
+        <div className="absolute inset-0 w-full h-full" style={{ backgroundColor: '#271D0099' }}></div>
         <div className="relative z-10 h-full flex flex-col items-center justify-center text-theme-primary">
           <div className="absolute top-4 left-4">
             <BackButton color="text-theme-primary"/>
           </div>
-          <h1 className="absolute text-2xl font-bold top-5 text-center">{t('rankingPage.title')}</h1>
+          <h1 className="absolute text-2xl font-bold top-5 text-center text-white">{t('rankingPage.title')}</h1>
           <div className="text-center">
-            <p className="text-lg">&#34;Top Picks In Phenom Penh&#34;</p>
+            <p className="text-lg text-white">&#34;Top Picks In Phnom Penh&#34;</p>
           </div>
         </div>
       </div>
 
-      <div className="w-full max-w-[480px] mx-auto flex flex-col flex-1 bg-theme-primary rounded-t-3xl z-20 overflow-y-auto pt-5 sticky top-0">
-        <div className="sticky top-0 bg-theme-primary z-30">
-          <div className="px-5 mb-4 flex space-x-4">
-            <div className="flex-1">
-              <div 
-                className="relative w-full py-3 px-4 rounded-xl flex justify-between items-center cursor-pointer"
-                onClick={openRegionModal}
-                style={{ background: 'linear-gradient(#FFE3D4, #FEA269)' }}
-              >
-                <span style={{ color: '#581E00' }}>{country ? t(`regions.${country}`) : t('addLocation.selectCountryPlaceholder')}</span>
-                <span className="w-6 h-6 flex items-center justify-center rounded-full" style={{ backgroundColor: '#B15200' }}>
-                  <svg className="w-4 h-4 text-theme-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
-                  </svg>
-                </span>
-              </div>
-            </div>
-            <div className="flex-1">
-              <div
-                className={`relative w-full py-3 px-4 rounded-xl flex justify-between items-center cursor-pointer ${!country ? 'opacity-50 cursor-not-allowed' : ''}`}
-                onClick={openCityModal}
-                style={{ background: 'linear-gradient(268deg, #FFDF7D 5.8%, #FFF7DF 85.51%)' }}
-              >
-                 <span style={{ color: '#1A1300' }}>{state ? state : t('addLocation.selectStatePlaceholder')}</span>
-                 <span className="w-6 h-6 flex items-center justify-center rounded-full" style={{ backgroundColor: '#DAA400' }}>
-                   <svg className="w-4 h-4 text-theme-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
-                   </svg>
-                 </span>
-              </div>
-            </div>
-          </div>
-
-          <div className="px-5 flex space-x-3 mb-2">
-            {[t('rankingPage.popularTab'), t('rankingPage.mostViewTab'), t('rankingPage.trendingTab')].map((tab) => (
-              <button
-                key={tab}
-                className={`px-4 py-2 rounded-xl text-sm text-theme-primary ${activeTab === tab ? 'bg-[#5D4702]' : 'bg-theme-secondary'}`}
-                onClick={() => setActiveTab(tab)}
-              >
-                {tab}
-              </button>
-            ))}
-          </div>
-
-          <div className="px-5 flex items-center space-x-2 mb-1 overflow-x-auto no-scrollbar py-5">
-            {['Massage', 'Visa', 'Bar', 'Club'].map((service) => (
-              <button
-                key={service}
-                className={`px-5 py-2.5 rounded-xl text-sm flex-shrink-0 ${selectedService === service ? 'border border-[#886600] text-[#A27A00]' : 'text-theme-primary'}`}
-                onClick={() => setSelectedService(service)}
-              >
-                {service}
-              </button>
-            ))}
-            <button className="p-2 flex-shrink-0" onClick={openFilterPanel}>
-              <img src={TuneIcon} alt="Tune Icon" />
-            </button>
-          </div>
-        </div>
-
-        <div className="space-y-2 pt-5">
-          {rankingItems.map((item, index) => (
-            <div key={item.id} 
-              className="rounded-2xl overflow-hidden cursor-pointer mx-2"
-              onClick={() => navigate(`/location/${item.id}`)}
-            >
-              <div className="flex items-center p-2">
-                <div className="relative w-[150px] h-[120px] bg-theme-primary rounded-lg overflow-hidden mr-4">
-                  <div className="absolute top-0 left-0 w-10 h-10 bg-theme-primary bg-opacity-50 flex items-center justify-center rounded-br-lg">
-                    <span 
-                      className="text-transparent text-[28px] font-bold"
-                      style={{
-                        WebkitTextStrokeWidth: '1px',
-                        WebkitTextStrokeColor: 'var(--text-primary)'
-                      }}
-                    >
-                      {index + 1}
-                    </span>
-                  </div>
-                  <img 
-                    src={item.image} 
-                    alt={item.name} 
-                    className="w-full h-full object-cover"
-                  />
+      <div className=" w-full max-w-[480px] mx-auto flex flex-col flex-1 rounded-t-2xl z-20 overflow-y-auto">
+        <div className="pt-3">
+          <div className="bg-theme-primary z-30 sticky top-0 ">
+            <div className="px-3 mb-4 flex space-x-4">
+              <div className="flex-1">
+                <div 
+                  className="relative w-full py-3 px-4 rounded-xl flex justify-between items-center cursor-pointer"
+                  onClick={openRegionModal}
+                  style={{ background: 'linear-gradient(#FFE3D4, #FEA269)' }}
+                >
+                  <span style={{ color: '#581E00' }}>{country ? t(`regions.${country}`) : t('addLocation.selectCountryPlaceholder')}</span>
+                  <span className="w-6 h-6 flex items-center justify-center rounded-full" style={{ backgroundColor: '#B15200' }}>
+                    <svg className="w-4 h-4 text-theme-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
+                    </svg>
+                  </span>
                 </div>
-                <div className="flex-1">
-                  <div className="flex items-center justify-between mb-2">
-                    <h3 className="text-theme-primary text-lg font-semibold">{item.name}</h3>
-                  </div>
-                  <p className="text-theme-primary text-sm mb-2">{t('rankingPage.distance', { distance: item.distance })}</p>
-                  <div className="flex items-center gap-1 mb-2">
-                    <span className="text-[#FFC61B] text-sm">{item.rating}</span>
-                    {[...Array(5)].map((_, i) => (
-                      <svg key={i} className="w-4 h-4 text-[#FFC61B]" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                      </svg>
-                    ))}
-                    <span className="text-theme-primary text-sm">{t('rankingPage.reviews', { count: item.reviews })}</span>
-                  </div>
-                  {item.services && item.services.length > 0 && (
-                    <p className="text-theme-primary text-sm mb-2">{item.services.join(', ')}</p>
-                  )}
-                  <div className="flex items-center justify-between">
-                    <div className="text-[#FFC61B] font-semibold">{item.price}</div>
-                  </div>
+              </div>
+              <div className="flex-1">
+                <div
+                  className={`relative w-full py-3 px-4 rounded-xl flex justify-between items-center cursor-pointer ${!country ? 'opacity-50 cursor-not-allowed' : ''}`}
+                  onClick={openCityModal}
+                  style={{ background: 'linear-gradient(268deg, #FFDF7D 5.8%, #FFF7DF 85.51%)' }}
+                >
+                   <span style={{ color: '#1A1300' }}>{state ? state : t('addLocation.selectStatePlaceholder')}</span>
+                   <span className="w-6 h-6 flex items-center justify-center rounded-full" style={{ backgroundColor: '#DAA400' }}>
+                     <svg className="w-4 h-4 text-theme-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
+                     </svg>
+                   </span>
                 </div>
               </div>
             </div>
-          ))}
+
+            <div className="px-5 flex space-x-3 mb-2">
+              {[t('rankingPage.popularTab'), t('rankingPage.mostViewTab'), t('rankingPage.trendingTab')].map((tab) => (
+                <button
+                  key={tab}
+                  className={`px-4 py-2 rounded-xl text-sm text-theme-primary ${activeTab === tab ? 'bg-[#FFC61B]' : 'bg-theme-secondary'}`}
+                  onClick={() => setActiveTab(tab)}
+                >
+                  {tab}
+                </button>
+              ))}
+            </div>
+
+            <div className="px-5 flex items-center space-x-2 mb-1 overflow-x-auto no-scrollbar py-5">
+              {['Massage', 'Visa', 'Bar', 'Club'].map((service) => (
+                <button
+                  key={service}
+                  className={`px-5 py-2.5 rounded-xl text-sm flex-shrink-0 ${selectedService === service ? 'border border-[#886600] text-[#A27A00]' : 'text-theme-primary'}`}
+                  onClick={() => setSelectedService(service)}
+                >
+                  {service}
+                </button>
+              ))}
+              <button className="p-2 flex-shrink-0" onClick={openFilterPanel}>
+                <img src={TuneIcon} alt="Tune Icon" style={{ filter: 'var(--icon-filter)' }} />
+              </button>
+            </div>
+          </div>
+
+          <div className="space-y-2 pt-2">
+            {rankingItems.map((item, index) => (
+              <div key={item.id} 
+                className="rounded-2xl overflow-hidden cursor-pointer mx-2"
+                onClick={() => navigate(`/location/${item.id}`)}
+              >
+                <div className="flex items-center p-2">
+                  <div className="relative w-[150px] h-[120px] bg-theme-primary rounded-lg overflow-hidden mr-4">
+                    <div className="absolute top-0 left-0 w-10 h-10 bg-theme-primary bg-opacity-50 flex items-center justify-center rounded-br-lg">
+                      <span 
+                        className="text-transparent text-[28px] font-bold"
+                        style={{
+                          WebkitTextStrokeWidth: '1px',
+                          WebkitTextStrokeColor: 'var(--text-primary)'
+                        }}
+                      >
+                        {index + 1}
+                      </span>
+                    </div>
+                    <img 
+                      src={item.image} 
+                      alt={item.name} 
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                  <div className="flex-1">
+                    <div className="flex items-center justify-between mb-2">
+                      <h3 className="text-theme-primary text-lg font-semibold">{item.name}</h3>
+                    </div>
+                    <p className="text-theme-primary text-sm mb-2">{t('rankingPage.distance', { distance: item.distance })}</p>
+                    <div className="flex items-center gap-1 mb-2">
+                      <span className="text-[#FFC61B] text-sm">{item.rating}</span>
+                      {[...Array(5)].map((_, i) => (
+                        <svg key={i} className="w-4 h-4 text-[#FFC61B]" fill="currentColor" viewBox="0 0 20 20">
+                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                        </svg>
+                      ))}
+                      <span className="text-theme-primary text-sm">{t('rankingPage.reviews', { count: item.reviews })}</span>
+                    </div>
+                    {item.services && item.services.length > 0 && (
+                      <p className="text-theme-primary text-sm mb-2">{item.services.join(', ')}</p>
+                    )}
+                    <div className="flex items-center justify-between">
+                      <div className="text-[#FFC61B] font-semibold">{item.price}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       {showRegionModal && <RegionSelectModal onSelectRegion={handleRegionSelect} onClose={closeRegionModal} />}
