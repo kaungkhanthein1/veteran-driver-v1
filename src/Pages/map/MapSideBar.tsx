ed1aa42bfa90aaf39f324b02356f08a7745ee0ee
hlaing-dev <hlaingmin1235145@gmail.com>
2025-07-08 21:53:25 +0700

    update

--- src/Pages/map/MapSideBar.tsx
+++ src/Pages/map/MapSideBar.tsx
@@ -14,17 +14,14 @@ const MapSideBar = ({ onRecenterClick, isExpanded }: MapSideBarProps) => {
   if (isExpanded) return null;
 
   return (
-    <div className="absolute right-4 flex flex-col gap-3 z-50 top-64">
+    <div className="absolute right-4 flex flex-col gap-3 z-50 bottom-64">
       <button
         onClick={onRecenterClick}
         className="w-10 h-10 rounded-full bg-white shadow-lg flex items-center justify-center"
       >
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-          <path
-            d="M12 8C9.79 8 8 9.79 8 12C8 14.21 9.79 16 12 16C14.21 16 16 14.21 16 12C16 9.79 14.21 8 12 8ZM20.94 11C20.48 6.83 17.17 3.52 13 3.06V2C13 1.45 12.55 1 12 1C11.45 1 11 1.45 11 2V3.06C6.83 3.52 3.52 6.83 3.06 11H2C1.45 11 1 11.45 1 12C1 12.55 1.45 13 2 13H3.06C3.52 17.17 6.83 20.48 11 20.94V22C11 22.55 11.45 23 12 23C12.55 23 13 22.55 13 22V20.94C17.17 20.48 20.48 17.17 20.94 13H22C22.55 13 23 12.55 23 12C23 11.45 22.55 11 22 11H20.94ZM12 19C8.13 19 5 15.87 5 12C5 8.13 8.13 5 12 5C15.87 5 19 8.13 19 12C19 15.87 15.87 19 12 19Z"
-            fill="#275FFC"
-          />
-        </svg>
+        <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" clipRule="evenodd" d="M9.69325 10.6381C9.69325 10.6381 -2.2834 8.16057 1.87855 5.75802C5.3907 3.73072 17.4946 0.245168 19.1856 1.14577C20.0862 2.83677 16.6006 14.9407 14.5733 18.4528C12.1709 22.6148 9.69325 10.6381 9.69325 10.6381Z" stroke="#444444" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
       </button>
     </div>
   );
