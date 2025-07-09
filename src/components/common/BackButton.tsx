1157226a24dd91e0047f3ef0a37eacb846a8a2d8
hlaing-dev <hlaingmin1235145@gmail.com>
2025-07-09 20:35:18 +0700

    update

--- src/components/common/BackButton.tsx
+++ src/components/common/BackButton.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import ArrowBackIcon from "icons/arrow_back.webp";
 import { useTranslation } from "react-i18next";
 import PropTypes from "prop-types";
